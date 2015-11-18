26bf28d95adecab3c10339d06ac788416e230840
Karla Holman <holmankarla@gmail.com>
2015-11-18 13:06:40 -0800

    Added seeds for products and applications

--- db/default/brand_model_application.rb
+++ db/default/brand_model_application.rb
@@ -51,4 +51,112 @@ make5.models.create :name => "LeBaron", :start_year => "1957", :end_year => "197
 
 # product_application = Spree::ProductApplication.create :application => application1, :start_year => 1956, :end_year => 1956 
 
+#-------------------------------------------------------------------------------------
+# Spree Set Up
+#-------------------------------------------------------------------------------------
 
+# Create Zones
+# What other zones do we need?
+
+# Create Tax Category (group products in same tax group)
+auto_tax_category = Spree::TaxCategory.create :name => "Auto Parts", :description => "Tax category for vintage automobile parts", :is_default => true, :tax_code => "CAR_TAX"
+
+# Create Tax Rates
+us_goods_rate = Spree::TaxRate.create :amount => BigDecimal.new("0.08"), :zone_id => 2, :tax_category_id => auto_tax_category.id, :included_in_price => false, :name => "Taxable US Goods", :show_rate_in_label => true
+
+# Create Shipping Category
+default = Spree::ShippingCategory.create :name => "Default"
+over_size = Spree::ShippingCategory.create :name => "Over-sized"
+
+# Create Shipping Methods (options when customer reaches checkout)
+ups_two_day = Spree::ShippingMethod.create :name => "UPS Two-Day", :tracking_url => "http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=:tracking", :admin_name => "UPS-2-DAY", :tax_category_id => auto_tax_category.id, :code => "UPS2DAY"
+
+# Create Body Type Taxonomies
+part_number = Spree::Property.create :name => "Part Number", :presentation => "Part Number"
+cast_number = Spree::Property.create :name => "Cast Number", :presentation => "Cast Number"
+cross_reference = Spree::Property.create :name => "Cross Reference", :presentation => "Cross Reference"
+
+# Create Test Part
+water_pump = Spree::Product.create :name => "1957-58 392 Hemi, 354 Poly Water Pump", 
+								   :description => "Water pump for all 1957 Chrysler New Yorker - Saratoga - Town/Country - Windsor - 300C - 300D and all 1957-58 Imperial.",
+								   :available_on => DateTime.new(2015,1,1),
+								   :slug => "1957-58-392-hemi-354-poly-water-pump",
+								   :tax_category_id => auto_tax_category.id, 
+								   :shipping_category_id => default.id,
+								   :promotionable => true,
+								   :price => 120.00
+
+tie_rod = Spree::Product.create :name => "Outer Tie Rod End - Long Wheel Base", 
+								 :description => "Fits right and left",
+								 :available_on => DateTime.new(2015, 1, 1),
+								 :slug => "outer-tie-rod-end-lwb",
+								 :tax_category_id => auto_tax_category.id, 
+								 :shipping_category_id => default.id,
+								 :promotionable => true,
+								 :price => 125.00
+
+# Create Product Variants
+tie_rod_variant = Spree::Variant.create :sku => "1692688", 
+								 :weight => BigDecimal.new("0"),
+								 :is_master => true,
+								 :product => tie_rod,
+								 :position => 1, 
+								 :cost_currency => "USD",
+								 :track_inventory => true,
+								 :stock_items_count => 0
+
+water_pump_variant = Spree::Variant.create :sku => "1692799", 
+								 :weight => BigDecimal.new("0"),
+								 :is_master => true,
+								 :product => water_pump,
+								 :position => 1, 
+								 :cost_currency => "USD",
+								 :track_inventory => true,
+								 :stock_items_count => 0
+
+# Create prices
+tie_price = Spree::Price.create :variant => tie_rod_variant,
+							:amount => 125.00,
+							:currency => "USD"
+pump_price = Spree::Price.create :variant => water_pump_variant,
+							:amount => 120.00,
+							:currency => "USD"
+
+# Create Product Properties
+# Part numbers
+water_part_num = Spree::ProductProperty.create :value => "1692799",
+											   :product_id => water_pump.id,
+											   :property_id => part_number.id
+
+tie_rod_part_num = Spree::ProductProperty.create :value => "1692688",
+												 :product_id => tie_rod.id,
+												 :property_id => part_number.id
+
+# Cross References
+tie_rod_cross_ref = Spree::ProductProperty.create :value => "2279992, 2084353",
+												  :product_id => tie_rod.id,
+												  :property_id => cross_reference.id
+
+# Create applications
+
+# Chrysler 300F for product 1
+app1 = Spree::ProductApplication.create :product => tie_rod,
+										:application_id => 10,
+										:start_year => 1960,
+										:end_year => 1960
+
+app2 = Spree::ProductApplication.create :product => tie_rod,
+										:application_id => 9,
+										:start_year => 1959,
+										:end_year => 1959
+
+app3 = Spree::ProductApplication.create :product => water_pump,
+										:application_id => 9,
+										:start_year => 1959,
+										:end_year => 1959
+
+# Desoto no model
+app4 = Spree::ProductApplication.create :product => water_pump,
+										:application_id => 4,
+										:start_year => 1959,
+										:end_year => 1959
