23e4d254fdd5fc5d7f3614a7477449c058422cc0
Karla Holman <holmankarla@gmail.com>
2016-01-15 13:46:43 -0800

    Removed project images now on aws, fix javascript bug for gallery

--- app/assets/javascripts/image-hover.js
+++ app/assets/javascripts/image-hover.js
@@ -239,13 +239,14 @@ function advanceGallery(current, next, mobile) {
 		// Display placeholder to prevent div collapse
 		$gallery_placeholder.css("display", "inline");
 
-	}).load().delay(200);
+	}).load( function() { }).delay(200);
 
-	$gallery_main.fadeToggle( { duration: 400, start: function() {
-	// Hide placeholder when image starts fading in
-	$gallery_placeholder.css("display", "none");
+	$gallery_main.fadeToggle( 400, function() {
+		// Hide placeholder when image starts fading in
+		$gallery_placeholder.css("display", "none");
 
-	next.closest("div").addClass("thumb_selected");
+		next.closest("div").addClass("thumb_selected");
+	});
 }
 
 // Update main gallery image and caption
