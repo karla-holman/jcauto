f86f4b24e726e567d1cf25e0d8f7d9cf2a0dcfe4
Karla Holman <holmankarla@gmail.com>
2016-01-15 13:57:30 -0800

    Moved placeholder hide

--- app/assets/javascripts/image-hover.js
+++ app/assets/javascripts/image-hover.js
@@ -239,12 +239,11 @@ function advanceGallery(current, next, mobile) {
 		// Display placeholder to prevent div collapse
 		$gallery_placeholder.css("display", "inline");
 
-	}).load( function() { }).delay(200);
+	}).load( function() { }).delay(100);
 
-	// Hide placeholder when image starts fading in
-	$gallery_placeholder.css("display", "none");
-
-	$gallery_main.fadeToggle( 400, function() {
+	$gallery_main.delay(200).fadeToggle( 400, function() {
+		// Hide placeholder when image starts fading in
+		$gallery_placeholder.css("display", "none");
 		next.closest("div").addClass("thumb_selected");
 	});
 }
