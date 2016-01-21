97c17155d9f08a11b72fd94f17cc8d5e4a544ad5
Karla Holman <holmankarla@gmail.com>
2016-01-21 14:22:02 -0800

    Added shipments to tracked obejcts for wombat

--- config/initializers/wombat.rb
+++ config/initializers/wombat.rb
@@ -2,11 +2,12 @@ Spree::Wombat::Config.configure do |config|
   config.connection_token = ENV['X_HUB_STORE']
   config.connection_id = ENV['X_HUB_ACCESS_TOKEN']
 
-  config.push_objects = ["Spree::Order", "Spree::Product"]
+  config.push_objects = ["Spree::Order", "Spree::Product", "Spree::Shipment"]
 
   config.payload_builder = {
 	  "Spree::Order"  => {:serializer => "Spree::Wombat::OrderSerializer", :root => "orders"},
-	  "Spree::Product" => {:serializer => "Spree::Wombat::ProductSerializer", :root => "products"}
+	  "Spree::Product" => {:serializer => "Spree::Wombat::ProductSerializer", :root => "products"},
+	  "Spree::Shipment" => {:serializer => "Spree::Wombat::ShipmentSerializer", :root => "shipments"}
 	}
 
 end
