06b7f76a5c2e00ddde7d395f27e9a8a32dc75b47
Karla Holman <holmankarla@gmail.com>
2016-01-21 16:46:21 -0800

    Removed shipments

--- config/initializers/wombat.rb
+++ config/initializers/wombat.rb
@@ -2,12 +2,11 @@ Spree::Wombat::Config.configure do |config|
   config.connection_token = ENV['X_HUB_STORE']
   config.connection_id = ENV['X_HUB_ACCESS_TOKEN']
 
-  config.push_objects = ["Spree::Order", "Spree::Product", "Spree::Shipment"]
+  config.push_objects = ["Spree::Order", "Spree::Product"]
 
   config.payload_builder = {
 	  "Spree::Order"  => {:serializer => "Spree::Wombat::OrderSerializer", :root => "orders"},
-	  "Spree::Product" => {:serializer => "Spree::Wombat::ProductSerializer", :root => "products"},
-	  "Spree::Shipment" => {:serializer => "Spree::Wombat::ShipmentSerializer", :root => "shipments"}
+	  "Spree::Product" => {:serializer => "Spree::Wombat::ProductSerializer", :root => "products"}
 	}
 
 end
