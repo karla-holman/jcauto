fe55efb3c422b2d2073b706a15bcfebb9e7db327
Karla Holman <holmankarla@gmail.com>
2016-01-26 17:24:50 -0800

    Added instruction images for quickbooks and whodoneit method for PaperTrail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_paper_trail_whodunnit
+
+  def user_for_paper_trail
+  	(spree_current_user) ? spree_current_user.id : "Public User (not logged in or no email)"
+  end
 end
