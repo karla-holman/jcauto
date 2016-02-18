49bd9f34842bcc05d2821aa26a73bcc6c0782caf
Karla Holman <holmankarla@gmail.com>
2016-02-18 13:22:24 -0800

    Fixed jquery for non-gallery pages, added arrows for sale cars

--- app/assets/javascripts/image-hover.js
+++ app/assets/javascripts/image-hover.js
@@ -1,33 +1,3 @@
-$("img").mouseenter(function(event){
-	// var image_src = $(this).attr("data");
-	//console.log($(this).attr("src"));
-
-	/*
-
-	// parse file structure
-	var file_name = image_src.split(".");
-	file_name_first = file_name[0].split("-");
-
-	var new_file_name = file_name_first + "-hover" + "." + file_name[1]; */
-	//hover_src = $(this).attr("data-hover")
-	//console.log(hover_src);
-	//$(this).attr("src", hover_src);
-
-}).mouseleave(function(event){
-	/* var image_src = $(this).attr("src");
-	console.log(image_src);
-
-	// parse file structure
-	var file_name = image_src.split("-hover");
-
-	var new_file_name = file_name[0] + file_name[1];
-	console.log(new_file_name); */
-
-	/*hover_src = $(this).attr("data.data_src")
-	$(this).attr("src", hover_src);*/
-
-});
-
 /* Handle Buttons */
 
 // Change to red on hover
@@ -156,25 +126,27 @@ $("#mobile-right").click(function(event) {
 // Swipe Left Event
 
 $(document).ready(function() {
-	var hammertime = new Hammer($('.gallery_container_mobile').get(0));
+	var hammertime; 
 
-	hammertime.on('swipe', function(ev) {
-		ev.srcEvent.preventDefault();
-		console.log(ev);
-		console.log("Direction: " + ev.direction);
-		if(ev.direction == 2) {
-			event.preventDefault();
-			$(this).delay(400);
-			advanceNext(true);
-		} else if (ev.direction == 4) {
-			event.preventDefault();
-			$(this).delay(400);
-			advancePrev(true);
-		}
-	});
+	if ($('.gallery_container_mobile').length > 0) {
+		var hammertime = new Hammer($('.gallery_container_mobile').get(0));
+	
+		hammertime.on('swipe', function(ev) {
+			ev.srcEvent.preventDefault();
+			// console.log(ev);
+			// console.log("Direction: " + ev.direction);
+			if(ev.direction == 2) {
+				event.preventDefault();
+				$(this).delay(400);
+				advanceNext(true);
+			} else if (ev.direction == 4) {
+				event.preventDefault();
+				$(this).delay(400);
+				advancePrev(true);
+			}
+		});
+	}
 });
-
-// $( window ).on( "swipe", function( event ) { console.log('other swipe'); } )
 
 /* Helper functions */
 
