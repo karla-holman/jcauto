baba03ed3677fdd79bed19ce22af7a8a00b4596d
Karla Holman <holmankarla@gmail.com>
2016-03-04 11:53:58 -0800

    Added js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets 
+//= require bootstrap-sprockets
 //= require spree
 //= require spree/frontend/cart
 //= require spree/frontend/checkout
