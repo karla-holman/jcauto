c1f63a0999af7c5d09d6b54728c8ba5031ac135a
Karla Holman <holmankarla@gmail.com>
2016-03-04 15:12:24 -0800

    fixed current user role issue

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_filter :authorized?
   private
   def authorized?
-    unless spree_current_user && spree_current_user.spree_roles.first.name == "admin"
+    unless spree_current_user && spree_current_user.has_spree_role? "admin"
       flash[:error] = "You are not authorized to view that page. Please log in to continue."
       redirect_to spree_login_path
     end
