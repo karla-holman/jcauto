54a97095c2027abf66f61c663f46176cac5ac864
Karla Holman <holmankarla@gmail.com>
2016-03-07 15:48:43 -0800

    Revert mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,17 +67,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => "jcauto.herokuapp.com" }
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: 'gmail.com',
-    user_name: 'jcautorestorationinc',
-    password: ENV['GMAIL_PASSWORD'],
-    authentication: :plain,
-    enable_starttls_auto: true,
-    tls: true 
-  }
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
