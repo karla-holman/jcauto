0ac016031810bc558d08aa2d283758f5e2c9a10f
Karla Holman <holmankarla@gmail.com>
2017-10-17 16:07:53 -0700

    change size back

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -17,9 +17,9 @@ end
 
 if Rails.env.production?
   Sidekiq.configure_server do |config|
-   config.redis = { url: ENV["REDISTOGO_URL"], size: 2 }
+   config.redis = { url: ENV["REDISTOGO_URL"] }
   end
   Sidekiq.configure_client do |config|
-   config.redis = { url: ENV["REDISTOGO_URL"], size: 1}
+   config.redis = { url: ENV["REDISTOGO_URL"] }
   end
 end
