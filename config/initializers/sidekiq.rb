9ec0f17ed74c75c46fe1e9c25a48f040d67c78d4
Karla Holman <holmankarla@gmail.com>
2017-10-17 16:04:49 -0700

    configure redis pool

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,6 +9,12 @@ end
 
 =end
 
+require 'sidekiq/web'
+
+Sidekiq.configure_server do |config|
+ActiveRecord::Base.configurations[Rails.env.to_s]['pool'] = 30
+end
+
 if Rails.env.production?
   Sidekiq.configure_server do |config|
    config.redis = { url: ENV["REDISTOGO_URL"], size: 2 }
