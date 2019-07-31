019672cb52a5e71a7ede37f1a6a7a818d8fb7ee5
Brady Hislop <hislopbrady@gmail.com>
2019-07-31 15:01:40 -0700

    Ensuring that the key task works

--- app/controllers/shop_images_controller.rb
+++ app/controllers/shop_images_controller.rb
@@ -1,6 +1,6 @@
 #Controls the images for the users of the website 
 #allowing them to see the shop images that have been added to the webpage.
-#Another comment to allow me to push.
+#Another comment to allow me to push heroku hopefully this step works.
 class ShopImagesController < ApplicationController
   def show
      @shop_image = ShopImage.all 
