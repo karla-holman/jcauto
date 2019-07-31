0ef57f78b34c0f5c5dd7726f682ebdf4c5ff7b7c
Brady Hislop <hislopbrady@gmail.com>
2019-07-31 08:27:20 -0700

    Redis changes working on upload

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 # $redis = Redis.new(url: ENV["REDIS_URL"])
 if Rails.env.production?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:url => uri)
+    $redis = Redis.new(url: ENV["REDIS_URL"])
+    #uri = URI.parse(ENV["REDISTOGO_URL"])
+    #REDIS = Redis.new(:url => uri)
 end
