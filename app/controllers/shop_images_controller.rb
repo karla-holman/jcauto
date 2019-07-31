7726c386a6da7f3f3ebf5027be6f2b459970ecea
Brady Hislop <hislopbrady@gmail.com>
2019-07-31 15:18:50 -0700

    Changing of the path callout

--- app/controllers/shop_images_controller.rb
+++ app/controllers/shop_images_controller.rb
@@ -1,6 +1,6 @@
 #Controls the images for the users of the website 
 #allowing them to see the shop images that have been added to the webpage.
-#Another comment to allow me to push heroku hopefully this step works.
+#Another comment to allow me to push heroku hopefully this step.
 class ShopImagesController < ApplicationController
   def show
      @shop_image = ShopImage.all 
