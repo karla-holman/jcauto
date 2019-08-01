cf1df99588c4a1a30a0756b7b9b7490ae1473ff6
Brady Hislop <hislopbrady@gmail.com>
2019-08-01 09:11:34 -0700

    Type Error causing issues while reading the file

--- app/controllers/shop_images_controller.rb
+++ app/controllers/shop_images_controller.rb
@@ -1,8 +1,4 @@
 #Controls the images for the users of the website 
-#allowing them to see the shop images that have been added to the webpage.
-#Another comment to allow me to push heroku hopefully. Reading further on how to
-#upload and get the file. Now seeing if I get the TypeError again after getting rid of the read portion of the open spreadsheet.
-
 
 class ShopImagesController < ApplicationController
   def show
