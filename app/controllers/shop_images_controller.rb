af4f79cad50a81ab5461cab7cd5cc2b1b80ea800
Brady Hislop <hislopbrady@gmail.com>
2019-08-02 14:47:14 -0700

    Attempting to load the file before trying to save it.

--- app/controllers/shop_images_controller.rb
+++ app/controllers/shop_images_controller.rb
@@ -11,6 +11,8 @@ class ShopImagesController < ApplicationController
   #If I can just locate the location of the saved file I will be able to do what I need to do.
   #Is the file being sent to AWS?
   #If so how can I call out this file allowing me to open it.
+  #What am I missing about the saving process?
+  #Was there a parameter set somewhere specifying how to save the excel upload?
   private
   
   def set_shop_image
