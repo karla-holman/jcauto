319e30bccabcd286ffc4b301d425ee07de8563d5
Brady Hislop <hislopbrady@gmail.com>
2019-08-05 16:45:25 -0700

    I may have found the path to the excel file

--- app/controllers/shop_images_controller.rb
+++ app/controllers/shop_images_controller.rb
@@ -39,6 +39,7 @@ class ShopImagesController < ApplicationController
   #The reading of the spreadsheet doesnt seem to be working.
   #This should read the file as needed I need to get passed the private methods
   #I think that it would be best to move to a text file.
+  #Finally I may have solved the issue of where the spreadsheet is located.
 
 
 
