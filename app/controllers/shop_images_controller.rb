c61565c2135ff2ee3ba163fe128d61e9abee873a
Brady Hislop <hislopbrady@gmail.com>
2019-08-05 15:48:26 -0700

    Seeing if read works to read without causing a security issue.

--- app/controllers/shop_images_controller.rb
+++ app/controllers/shop_images_controller.rb
@@ -37,7 +37,7 @@ class ShopImagesController < ApplicationController
   #Now trying to see if the name issue has to do with the amazon s3 path or the object not  
   #I am starting to get a little bit frustrated that this is not working how I was hoping it would.
   #The reading of the spreadsheet doesnt seem to be working.
-  #This should read the file as needed.
+  #This should read the file as needed I need to get passed the private methods
 
 
 
