f184ee28a7912b0d914fac85c58f8c30facf9dd8
Brady Hislop <hislopbrady@gmail.com>
2019-08-06 09:39:15 -0700

    Added to the general_settings controller

--- app/controllers/shop_images_controller.rb
+++ app/controllers/shop_images_controller.rb
@@ -1,5 +1,6 @@
 #Controls the images for the users of the website
 #I have found out how to call out the temp file each time allowing for the opening of the spreadsheet.
+
 class ShopImagesController < ApplicationController
   def show
      @shop_image = ShopImage.all 
