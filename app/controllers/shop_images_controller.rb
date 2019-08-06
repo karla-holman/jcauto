3600bce8f4392ac5a12034a1b8df5fc2449c846f
Brady Hislop <hislopbrady@gmail.com>
2019-08-06 10:00:30 -0700

    Working on the redirect command to send the appropriate data to the new product controller

--- app/controllers/shop_images_controller.rb
+++ app/controllers/shop_images_controller.rb
@@ -1,6 +1,7 @@
 #Controls the images for the users of the website
 #I have found out how to call out the temp file each time allowing for the opening of the spreadsheet.
-
+#Trying to send the data to the new product controller
+#when using the redirect_to do I need the format.html
 class ShopImagesController < ApplicationController
   def show
      @shop_image = ShopImage.all 
