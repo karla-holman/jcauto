64fddb259aaa70f6e7eb7cae043984eb2103a982
Brady Hislop <hislopbrady@gmail.com>
2019-08-05 17:49:28 -0700

    Opening and parsing spreadsheet

--- app/controllers/shop_images_controller.rb
+++ app/controllers/shop_images_controller.rb
@@ -1,6 +1,6 @@
 #Controls the images for the users of the website
 #I have found out how to call out the temp file each time allowing for the opening of the spreadsheet.
-#I need to slow down and make the approprate changes before simply tryin the next commit
+#I need to slow down and make the approprate changes before simply tryin the next commit.
 class ShopImagesController < ApplicationController
   def show
      @shop_image = ShopImage.all 
