50b10d0a0b4f039c4539870a838a0304a7446ec1
Brady Hislop <hislopbrady@gmail.com>
2019-08-06 08:36:28 -0700

    Changing the copyright date to match the latest updates to the website.

--- app/controllers/shop_images_controller.rb
+++ app/controllers/shop_images_controller.rb
@@ -1,6 +1,5 @@
 #Controls the images for the users of the website
 #I have found out how to call out the temp file each time allowing for the opening of the spreadsheet.
-#Now that I have opened the spreadsheet I will focus on sending this data to the controller to create the product
 class ShopImagesController < ApplicationController
   def show
      @shop_image = ShopImage.all 
